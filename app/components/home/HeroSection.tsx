e0ee7aacb112d4d326be70eeb48dbc2ddd3926e9
Moussa Gaye <moussgaye0@gmail.com>
2025-07-07 11:14:55 +0100

    Fix TypeScript JSX syntax error in HeroSection

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -120,6 +120,5 @@ export function HeroSection() {
       {/* Bottom gradient */}
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white to-transparent"></div>
     </section>
-  )
   );
 }
